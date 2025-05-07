e32d112ba6f2215eeebe4809b0e6073efd6cac76
Yujin KIM <zla_db@naver.com>
2025-05-07 15:51:55 +0900

    feat: ApiResponse 추가, 로거 설정 및 에러 핸들러 추가

---
+++ backend/boindang-user/src/main/java/com/boindanguser/common/model/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.boindanguser.common.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
+}
