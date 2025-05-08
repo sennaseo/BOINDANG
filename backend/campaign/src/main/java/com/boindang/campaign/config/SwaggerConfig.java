09c6906b259d109ec452a202ea70847b1943f44f
ì Nageum Jeong <nagum0545@naver.com>
2025-05-08 17:57:28 +0900

    feat: SwaggerConfig 추가 및 application.yml 코드 수정

---
+++ backend/campaign/src/main/java/com/boindang/campaign/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.boindang.campaign.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${openapi.service.url}") String url) {
+		return new OpenAPI()
+			.servers(List.of(new Server().url(url)))
+			.info(new Info().title("Encyclopedia Service API")
+				.version("v0.0.1"));
+	}
+}
+
