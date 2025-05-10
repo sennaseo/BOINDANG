5a4f272dea234b0969eb2df38d838ccf414a2522
김휘동 <rlagnlehd0417@gmail.com>
2025-05-10 23:37:21 +0900

    feat: 인증서버 연동 구현

---
+++ backend/auth/src/main/java/com/d206/auth/dto/JwtTokenDto.java
@@ -0,0 +1,17 @@
+package com.d206.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtTokenDto {
+    private String refreshToken;
+    private String accessToken;
+}
