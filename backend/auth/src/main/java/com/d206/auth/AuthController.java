b7676c0643554d9a50eee062ae9957c7d900d0c5
김휘동 <rlagnlehd0417@gmail.com>
2025-05-11 02:51:35 +0900

    feat: 토큰 검증 추가

--- backend/auth/src/main/java/com/d206/auth/AuthController.java
+++ backend/auth/src/main/java/com/d206/auth/AuthController.java
@@ -1,15 +1,16 @@
 package com.d206.auth;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.d206.auth.common.ApiResponses;
 import com.d206.auth.dto.ApiResponse;
 import com.d206.auth.dto.JwtTokenDto;
-import com.d206.auth.security.JwtTokenProvider;
+import com.d206.auth.dto.ValidateDto;
+import com.d206.auth.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,16 +18,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-	private final JwtTokenProvider jwtTokenProvider;
+	private final AuthService authService;
 
 	@GetMapping("/createToken/{userId}")
-	public ApiResponse<JwtTokenDto> createAccessToken(@PathVariable Long userId) {
-		System.out.println("Request: " + userId);
-		JwtTokenDto jwtTokenDto = JwtTokenDto.builder()
-			.accessToken(jwtTokenProvider.createAccessToken(userId))
-			.refreshToken(jwtTokenProvider.createRefreshToken())
-			.build();
-		return ApiResponse.success(jwtTokenDto);
+	public ApiResponse<JwtTokenDto> createToken(@PathVariable Long userId) {
+		return ApiResponse.success(authService.createToken(userId));
+	}
+
+	@PostMapping("/validate")
+	public ApiResponse<Long> validateToken(@RequestBody ValidateDto validateDto) {
+		System.out.println("request token: " + validateDto.getAccessToken());
+		return ApiResponse.success(authService.validateToken(validateDto.getAccessToken()));
 	}
 
 }
