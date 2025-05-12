f34f05f3c93b9d439b60966c0373b8de08901fd8
김휘동 <rlagnlehd0417@gmail.com>
2025-05-12 14:04:30 +0900

    fix: env 추가

--- backend/ocr-service/app/config.py
+++ backend/ocr-service/app/config.py
@@ -5,8 +5,14 @@ class Settings(BaseSettings):
     CLOVA_OCR_API_URL: str
     CLOVA_OCR_SECRET_KEY: str
     OPENAI_API_URL: str
-    MONGODB_URI: str
     OPENAI_API_KEY: str
+    MONGODB_URI: str
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    EUREKA_URL: str
+    HOST_IP:str
+
 
     # model_config = SettingsConfigDict(env_file="../../.env")  # ✅ 여기 수정!
     model_config = SettingsConfigDict(env_file=".env_ocr")
