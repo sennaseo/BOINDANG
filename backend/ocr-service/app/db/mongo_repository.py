a6bd21581e1ab372eac6037277d3e6591afc8059
김휘동 <rlagnlehd0417@gmail.com>
2025-05-14 00:08:23 +0900

    fix: 엔드포인트 변경

--- backend/ocr-service/app/db/mongo_repository.py
+++ backend/ocr-service/app/db/mongo_repository.py
@@ -3,11 +3,10 @@ from app.config import get_settings
 from urllib.parse import quote_plus
 import os
 
-settings = get_settings()
 username = quote_plus(os.environ["DB_USERNAME"])    # url에 특수문자가 들어가면 안되기 때문에 변환해줌
 password = quote_plus(os.environ["DB_PASSWORD"])
 dbname = os.environ["DB_NAME"]
-MONGODB_URI = f"mongodb://{username}:{password}@ocr-db:27017/{dbname}"
+MONGODB_URI = f"mongodb://{username}:{password}@ocr-db:27017/{dbname}?authSource=admin"
 
 client = MongoClient(MONGODB_URI)
 db = client[dbname]
@@ -16,7 +15,7 @@ print("Mongo URI:", MONGODB_URI)
 print("DB name:", db.name)
 
 def save_product(product_name, result):
-    print("🧪 save_product() 함수 시작")  # 제일 위에 추가해봐
+    print("🧪 save_product() 함수 시작")
     db.product.insert_one({
         "name": product_name,
         "result": result
