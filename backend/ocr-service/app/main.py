0383121963fdacf197ad6510d54d349cd134afe6
김휘동 <rlagnlehd0417@gmail.com>
2025-05-13 16:54:08 +0900

    fix: main 함수 수정

--- backend/ocr-service/app/main.py
+++ backend/ocr-service/app/main.py
@@ -1,6 +1,6 @@
 # app/main.py
-import os
 
+import os
 from fastapi import FastAPI
 from app.routes import ocr_router
 from py_eureka_client import eureka_client
@@ -8,12 +8,33 @@ from contextlib import asynccontextmanager
 import logging
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 origins = [
     "https://k12d206.p.ssafy.io",
     "http://localhost:3000"
 ]
 
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    host_ip = os.environ["HOST_IP"]
+    eureka_url = os.environ["EUREKA_URL"]
+    await eureka_client.init_async(
+        eureka_server=eureka_url,
+        app_name="ocr-service",
+        instance_port=8083,
+        instance_host=host_ip,
+        instance_ip=host_ip
+    )
+    logger.info("Eureka client initialized")
+    yield
+    logger.info("Stopping Eureka client")
+    await eureka_client.stop_async()
+
+app = FastAPI(lifespan=lifespan)
+
+# 미들웨어와 라우터 등록은 lifespan 아래에서!
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -23,38 +44,4 @@ app.add_middleware(
     max_age=3600,
 )
 
-
-
 app.include_router(ocr_router.router, prefix="/ocr")
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
-@asynccontextmanager
-async def lifespan(app_: FastAPI):
-    host_ip = os.environ["HOST_IP"]
-    eureka_url = os.environ["EUREKA_URL"]
-    await eureka_client.init_async(
-        eureka_server= eureka_url,
-        app_name="ocr-service",
-        instance_port=8083,
-        instance_host=host_ip,
-        instance_ip=host_ip
-    )
-    logger.info("Eureka client initialized")
-    yield
-    # Shutdown
-    logger.info("Stopping Eureka client")
-    await eureka_client.stop_async()
-
-
-app = FastAPI(lifespan=lifespan)
-
-# @app("startup")
-# async def startup_event():
-#     print("Routes:", app.routes)
-
-if __name__ == "__main__":
-    import uvicorn
-    # uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True) # 개발 시 주석 해제
