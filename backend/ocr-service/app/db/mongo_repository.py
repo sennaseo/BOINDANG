c0ede946aab1b9024df8a165adf98d41f25d92df
김휘동 <rlagnlehd0417@gmail.com>
2025-05-13 23:18:33 +0900

    fix: db설정 변경(url 인코딩 변경)

--- backend/ocr-service/app/db/mongo_repository.py
+++ backend/ocr-service/app/db/mongo_repository.py
@@ -1,15 +1,18 @@
 from pymongo import MongoClient
 from app.config import get_settings
+from urllib.parse import quote_plus
 import os
 
 settings = get_settings()
-
-MONGODB_URI = settings.MONGODB_URI
+username = quote_plus(os.environ["DB_USERNAME"])
+password = quote_plus(os.environ["DB_PASSWORD"])
+dbname = os.environ["DB_NAME"]
+MONGODB_URI = f"mongodb://{username}:{password}@ocr-db:27017/{dbname}"
 
 client = MongoClient(MONGODB_URI)
-db = client[os.environ["DB_NAME"]]
+db = client[dbname]
 
-print("Mongo URI:", os.environ["MONGODB_URI"])
+print("Mongo URI:", MONGODB_URI)
 print("DB name:", db.name)
 
 def save_product(product_name, result):
