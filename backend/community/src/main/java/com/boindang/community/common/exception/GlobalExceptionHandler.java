67600ed7090f8b0937a729f4bf58f1bfd46f0bab
ì Nageum Jeong <nagum0545@naver.com>
2025-05-14 23:51:36 +0900

    refactor: GlobalExceptionHandler에 @Valid 관련 에러처리 추가

--- backend/community/src/main/java/com/boindang/community/common/exception/GlobalExceptionHandler.java
+++ backend/community/src/main/java/com/boindang/community/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.boindang.community.common.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 
 import com.boindang.community.dto.response.BaseResponse;
 
@@ -11,6 +13,17 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(CommunityException.class)
 	public BaseResponse<?> handleIngredientException(CommunityException e) {
 		return BaseResponse.fail(e.getCode(), e.getMessage());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public BaseResponse<?> handleValidationException(MethodArgumentNotValidException ex) {
+		// 가장 첫 번째 에러 메시지만 추출
+		String message = ex.getBindingResult().getFieldErrors().stream()
+			.findFirst()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.orElse("유효성 검사 실패");
+
+		return BaseResponse.fail(400, message);
 	}
 
 	@ExceptionHandler(Exception.class)
