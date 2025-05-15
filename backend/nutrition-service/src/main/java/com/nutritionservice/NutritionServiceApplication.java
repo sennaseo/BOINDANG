8ea1e5bdb7c1633d86fc7ec6fda9f68860783a5d
Yujin KIM <zla_db@naver.com>
2025-05-15 11:19:16 +0900

    refactor: feign 코드 삭제

--- backend/nutrition-service/src/main/java/com/nutritionservice/NutritionServiceApplication.java
+++ backend/nutrition-service/src/main/java/com/nutritionservice/NutritionServiceApplication.java
@@ -2,10 +2,8 @@ package com.nutritionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.nutritionservice.nutrition.client") // UserClient 위치
 public class NutritionServiceApplication {
 
     public static void main(String[] args) {
