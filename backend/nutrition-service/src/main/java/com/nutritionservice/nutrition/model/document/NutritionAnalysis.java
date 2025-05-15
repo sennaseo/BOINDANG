a10ec52886740858c89964753d920c37eac28264
Yujin KIM <zla_db@naver.com>
2025-05-15 13:02:50 +0900

    refactor: Field 추가

--- backend/nutrition-service/src/main/java/com/nutritionservice/nutrition/model/document/NutritionAnalysis.java
+++ backend/nutrition-service/src/main/java/com/nutritionservice/nutrition/model/document/NutritionAnalysis.java
@@ -1,6 +1,7 @@
 package com.nutritionservice.nutrition.model.document;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
@@ -8,6 +9,8 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class NutritionAnalysis {
+    @Field("nutrition")
     private Nutrition nutrition;
+    @Field("summary")
     private String summary;
 }
