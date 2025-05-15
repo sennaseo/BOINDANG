73ef94bdf5ea4e8ffa35a7d5cd1a8ac23b9e20b7
ì Nageum Jeong <nagum0545@naver.com>
2025-05-15 17:09:39 +0900

    fix: 옵션 정렬 수정2

--- backend/quiz/src/main/java/com/boindang/quiz/infrastructure/QuizSolvedHistoryRepository.java
+++ backend/quiz/src/main/java/com/boindang/quiz/infrastructure/QuizSolvedHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.boindang.quiz.infrastructure;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,6 +18,8 @@ public interface QuizSolvedHistoryRepository extends JpaRepository<QuizSolvedHis
 	int countByUserIdAndIsCorrectTrue(Long userId);
 
 	List<QuizSolvedHistory> findAllByUserId(Long userId);
+
+	Optional<QuizSolvedHistory> findByUserIdAndQuizId(Long userId, Long quizId);
 
 }
 
