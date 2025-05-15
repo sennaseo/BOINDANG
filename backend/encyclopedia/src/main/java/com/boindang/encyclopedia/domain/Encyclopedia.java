cfb2d5ddbed44668f076633e26cb6cd96f885a41
ì Nageum Jeong <nagum0545@naver.com>
2025-05-15 16:13:15 +0900

    fix: Encyclopedia 엔티티 테이블 지정

--- backend/encyclopedia/src/main/java/com/boindang/encyclopedia/domain/Encyclopedia.java
+++ backend/encyclopedia/src/main/java/com/boindang/encyclopedia/domain/Encyclopedia.java
@@ -6,10 +6,12 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Getter
 @Entity
+@Table(name = "encyclopedia_json")
 public class Encyclopedia {
 	@Id
 	@GeneratedValue
