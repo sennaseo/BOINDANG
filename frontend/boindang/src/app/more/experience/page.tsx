0def179b161be19074772578209366c841e3b34c
권가영 <kky3653@naver.com>
2025-05-16 10:27:32 +0900

    [FE] 빌드오류

--- frontend/boindang/src/app/more/experience/page.tsx
+++ frontend/boindang/src/app/more/experience/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { ArrowLeft } from '@phosphor-icons/react';
 import BottomNavBar from "@/components/navigation/BottomNavBar";
