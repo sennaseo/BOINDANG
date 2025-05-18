f633f8c8e810a3d1e7167c46c883e1b101c14ff1
김휘동 <rlagnlehd0417@gmail.com>
2025-05-18 14:37:46 +0900

    fix: 응답 형식 수정

--- backend/nutrition-service/src/main/java/com/nutritionservice/nutrition/contoller/NutritionController.java
+++ backend/nutrition-service/src/main/java/com/nutritionservice/nutrition/contoller/NutritionController.java
@@ -1,6 +1,7 @@
 package com.nutritionservice.nutrition.contoller;
 
 import com.nutritionservice.common.model.dto.ApiResponse;
+import com.nutritionservice.common.model.dto.ApiResponses;
 import com.nutritionservice.nutrition.model.dto.response.NutritionReportHistoryResponse;
 import com.nutritionservice.nutrition.model.dto.response.NutritionReportResponse;
 import com.nutritionservice.nutrition.service.NutritionService;
@@ -17,26 +18,26 @@ public class NutritionController {
     private final NutritionService nutritionService;
 
     @GetMapping("/history")
-    public ApiResponse<List<NutritionReportHistoryResponse>> history(
+    public ApiResponses<List<NutritionReportHistoryResponse>> history(
             @RequestHeader("X-User-Id") String userId
     ) {
-        return ApiResponse.success(nutritionService.getUserReportHistory(userId));
+        return ApiResponses.success(nutritionService.getUserReportHistory(userId));
     }
 
     @GetMapping("/history/{productId}")
-    public ApiResponse<NutritionReportResponse> getDetailedReport(
+    public ApiResponses<NutritionReportResponse> getDetailedReport(
             @RequestHeader("X-User-Id") String userId,
             @PathVariable String productId
     ) {
-        return ApiResponse.success(nutritionService.getFullReportByProductId(userId, productId));
+        return ApiResponses.success(nutritionService.getFullReportByProductId(userId, productId));
     }
 
     @GetMapping("/analyze")
-    public ApiResponse<NutritionReportResponse> analyze(
+    public ApiResponses<NutritionReportResponse> analyze(
             @RequestHeader("X-User-Id") String userId,
             @RequestParam String productId
     ) {
         NutritionReportResponse response = nutritionService.analyzeProductForUser(userId, productId);
-        return ApiResponse.success(response);
+        return ApiResponses.success(response);
     }
 }
