0a7a69d7b102275d69d8af1183bbc9566b681fef
ì Nageum Jeong <nagum0545@naver.com>
2025-05-19 16:11:38 +0900

    refactor: 인기 검색어 조회 api에 ingredientId 데이터 추가

--- backend/encyclopedia/src/main/java/com/boindang/encyclopedia/presentation/dto/response/PopularIngredientResponse.java
+++ backend/encyclopedia/src/main/java/com/boindang/encyclopedia/presentation/dto/response/PopularIngredientResponse.java
@@ -1,6 +1,7 @@
 package com.boindang.encyclopedia.presentation.dto.response;
 
 public record PopularIngredientResponse(
+		String ingredientId,
         String ingredientName,
         long count
 ) {}
